01aeafee732aaeb56ecdef54035f769589e8a0b4
Danny Foster <danny@northwestern.edu>
2026-03-13 11:41:51 -0500

    chore: remove Percy visual testing and demo app

--- demo/config/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'paths' => [resource_path('views')],
-    'compiled' => storage_path('framework/views'),
-];
--- demo/routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Filament handles all routing via its panel provider.
